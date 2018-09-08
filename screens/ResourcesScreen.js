96c7478275cc99324f4a03064a084d6709a70327
warlyware <danward@gmail.com>
2018-09-08 17:18:20 -0400

    Build out parent ResourceScreen component

--- screens/ResourcesScreen.js
+++ screens/ResourcesScreen.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import React from 'react'
+import { ScrollView, StyleSheet } from 'react-native'
+import { List, ListItem } from 'react-native-elements'
+import axios from 'axios'
+import Image from 'react-native-remote-svg'
 
 export default class ResourcesScreen extends React.Component {
+  state = {
+    categories: []
+  }
   static navigationOptions = {
     title: 'Resources',
   };
 
+  componentDidMount = () => {
+    this.fetchResources()
+  }
+
+  fetchResources = async () => {
+    let res = await axios.get('http://localhost:5000/v1/tutorials/categories')
+    console.log(res.data)
+    this.setState({ categories: res.data.categories })
+  }
+
   render() {
     return (
       <ScrollView style={styles.container}>
-
+        <List containerStyle={{ marginBottom: 20 }}>
+          {this.state.categories.map(category => (
+            <ListItem
+              roundAvatar
+              key={category.title}
+              title={category.title}
+            />
+          ))}
+        </List>
       </ScrollView>
     );
   }
